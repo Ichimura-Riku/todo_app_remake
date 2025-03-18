85b7afc3005331ec09e869224c7eb19bc996304b
市村 凌久 <87126275+Ichimura-Riku@users.noreply.github.com>
2025-03-18 18:00:13 +0900

    Feature/issue4 (#19)
    * feat: topBar
    
    * feat: todo list preview
    
    * update: gitignore
    
    謎のideaファイルを追加

--- app/src/main/java/com/example/todoAppRemake/MainActivity.kt
+++ app/src/main/java/com/example/todoAppRemake/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.todoAppRemake.ui.screen.HomeScreen
 import com.example.todoAppRemake.ui.theme.TodoAppRemakeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,32 +23,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoAppRemakeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
+                AppScaffold(modifier = Modifier)
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun GreetingPreview() {
-    TodoAppRemakeTheme {
-        Greeting("Android")
+fun AppScaffold(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                colors =
+                TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Small Top App Bar")
+                },
+            )
+        },
+    ) { innerPadding ->
+        HomeScreen(modifier = Modifier.padding(innerPadding))
     }
 }
