edc2d67a19f9b1148ce28b7e8753e4ec029aeb62
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2025-03-25 17:25:31 +0900

    feat: notionDatabaseのテキストを画面に表示する

--- app/src/main/java/com/example/todoAppRemake/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/todoAppRemake/ui/screen/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.todoAppRemake.ui.screen
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,40 +8,68 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.todoAppRemake.ui.theme.TodoAppRemakeTheme
 import com.example.todoAppRemake.ui.viewmodel.HomeScreenViewModel
+import com.example.todoAppRemake.ui.viewmodel.NotionDatabaseUiState
 
 @Composable
 fun HomeScreen(
     viewModel: HomeScreenViewModel,
     modifier: Modifier = Modifier,
 ) {
-    val mockTodoPropertiesList =
-        List(5) { index ->
-            MockTodoCardProperties(
-                id = index,
-                text = "hello id: $index",
-            )
+    val notionDatabaseUiState = viewModel.notionDatabaseUiState
+    if (notionDatabaseUiState is NotionDatabaseUiState.Success) {
+        LazyColumn(
+            modifier =
+            modifier
+                .fillMaxSize()
+                .padding(start = 0.dp, top = 16.dp, end = 0.dp, bottom = 0.dp),
+        ) {
+            items(notionDatabaseUiState.databases.title) { title ->
+                TodoCard(
+                    modifier = Modifier,
+                    cardText = title.text.content,
+                    onClick = { Log.v("TodoCard", "click to id:${title.text.content}") },
+//                    onClick = { viewModel.fetchNotionDatabase() },
+                )
+            }
         }
+        notionDatabaseUiState.databases.title
+    } else {
+        val mockTodoPropertiesList =
+            List(5) { index ->
+                MockTodoCardProperties(
+                    id = index,
+                    text = "hello id: $index",
+                )
+            }
 
-    LazyColumn(
-        modifier =
-        modifier
-            .fillMaxSize()
-            .padding(start = 0.dp, top = 16.dp, end = 0.dp, bottom = 0.dp),
-    ) {
-        items(mockTodoPropertiesList) { mockTodoProperties ->
-            TodoCard(
-                modifier = Modifier,
-                cardText = mockTodoProperties.text,
+        LazyColumn(
+            modifier =
+            modifier
+                .fillMaxSize()
+                .padding(start = 0.dp, top = 16.dp, end = 0.dp, bottom = 0.dp),
+        ) {
+            items(mockTodoPropertiesList) { mockTodoProperties ->
+                TodoCard(
+                    modifier = Modifier,
+                    cardText = mockTodoProperties.text,
 //                onClick = { Log.v("TodoCard", "click to id:${mockTodoProperties.id}") },
-                onClick = { viewModel.fetchNotionDatabase() },
-            )
+                    onClick = { viewModel.fetchNotionDatabase() },
+                )
+            }
         }
     }
 }
@@ -68,13 +97,30 @@ fun TodoCard(
     }
 }
 
-// @Preview(showBackground = true)
-// @Composable
-// private fun GreetingPreview() {
-//    TodoAppRemakeTheme {
-//        AppScaffold(modifier = Modifier.fillMaxSize())
-//    }
-// }
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+private fun GreetingPreview() {
+    TodoAppRemakeTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar(
+                    colors =
+                    TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = {
+                        Text("Small Top App Bar")
+                    },
+                )
+            },
+        ) { innerPadding ->
+            HomeScreen(viewModel = HomeScreenViewModel(), modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
 
 data class MockTodoCardProperties(
     val id: Int,
--- app/src/main/java/com/example/todoAppRemake/ui/viewmodel/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/todoAppRemake/ui/viewmodel/HomeScreenViewModel.kt
@@ -1,26 +1,55 @@
 package com.example.todoAppRemake.ui.viewmodel
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todoAppRemake.BuildConfig
 import com.example.todoAppRemake.data.remote.NotionApiClient
+import com.example.todoAppRemake.data.remote.model.NotionDatabaseResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.io.IOException
 import javax.inject.Inject
+
+sealed interface NotionDatabaseUiState {
+    data class Success(
+        val databases: NotionDatabaseResponse,
+    ) : NotionDatabaseUiState
+
+    object Error : NotionDatabaseUiState
+
+    object Loading : NotionDatabaseUiState
+}
 
 @HiltViewModel
 class HomeScreenViewModel
 @Inject
 constructor() : ViewModel() {
+    var notionDatabaseUiState: NotionDatabaseUiState by mutableStateOf(NotionDatabaseUiState.Loading)
+        private set
+
+    init {
+        fetchNotionDatabase()
+    }
+
     fun fetchNotionDatabase() {
         viewModelScope.launch {
-            try {
-                val response = NotionApiClient.client.getDatabase(BuildConfig.NOTION_DB_ID)
-                Log.d("HomeScreenViewModel", "Response: $response")
-            } catch (e: Exception) {
-                Log.e("HomeScreenViewModel", "Error fetching Notion database$e")
-            }
+            notionDatabaseUiState =
+                try {
+                    notionDatabaseUiState = NotionDatabaseUiState.Loading
+                    val response = NotionApiClient.client.getDatabase(BuildConfig.NOTION_DB_ID)
+                    Log.d("HomeScreenViewModel", "Response: $response")
+                    NotionDatabaseUiState.Success(response)
+                } catch (e: IOException) {
+                    Log.e("HomeScreenViewModel", "IOException Error fetching Notion database$e")
+                    NotionDatabaseUiState.Error
+                } catch (e: Exception) {
+                    Log.e("HomeScreenViewModel", "Error fetching Notion database$e")
+                    NotionDatabaseUiState.Error
+                }
         }
     }
 }
