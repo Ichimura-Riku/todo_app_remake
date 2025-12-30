37eebb1143f97afc0ecf63eb27aed9d96974b92f
Ichimura-Riku <ichir438@gmail.com>
2025-12-31 08:42:52 +0900

    miscのフォーマットからgradle.ktsをgradleに変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ subprojects {
             endWithNewline()
         }
         format("misc") {
-            target("*.md", ".gitignore", "*.xml", "*.gradle.kts")
+            target("*.md", ".gitignore", "*.xml", "*.gradle")
             trimTrailingWhitespace()
             endWithNewline()
         }
